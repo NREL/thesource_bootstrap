d87df94a525805df7f470d259bc0b0ff838d0eca
Shaun Laws <Shaun.Laws@nrel.gov>
2021-11-17 10:25:02 -0700

    Add better comments.

--- js/global.js
+++ js/global.js
@@ -9,6 +9,11 @@
 
   Drupal.behaviors.thesource_bootstrap = {
     attach: function (context, settings) {
+      /* The Views Bootstrap accordions are missing the collapsed class when the page is first loaded. This is
+       * possibly a side-effect of using a template override to allow Views Bootstrap accordions to work with BS5 - @see
+       * https://www.drupal.org/project/views_bootstrap/issues/3168271 and
+       * templates/views_bootstrap/views-bootstrap-accordion.html.twig.
+       */
       $('button.accordion-button').addClass('collapsed');
     }
   };
